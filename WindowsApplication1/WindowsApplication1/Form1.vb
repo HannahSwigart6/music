82173c75d1236db16049db8189afaf55048350aa
HannahSwigart6 <hannahdeanswigart@gmail.com>
2016-10-27 11:17:08 -0500

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play("c:\users\hs131455\documents\visual studio 2013\Projects\WindowsApplication1\WindowsApplication1\Resources\My.Zelda.wav",
+                AudioPlayMode.BackgroundLoop)
+    End Sub
+End Class
+Public Class 
